6bb2f59cf53ccc1847d5d3d2416961ffe337bbff
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-10-26 12:54:10 +0300

    Add files via upload

---
+++ NUMBER4.py
@@ -0,0 +1,10 @@
+age=20
+price =1000.0
+name=("Samantha")
+Library_membership ="true"
+
+
+print(age,"is a type of", type (age))
+print(price,"is a type of", type (price))
+print(name, "is a type of", type (name))
+print(Library_membership, "is a type of", type (Library_membership))
