72438e900bbeca798f4d71a2fddaaa85e9d9cc56
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-10-26 12:46:44 +0300

    Add files via upload

---
+++ code operators.py
@@ -0,0 +1,48 @@
+a=3
+c=5
+s=3+5
+print(s)
+
+a=34
+b=5
+z=34%5
+print(z)
+
+J=5**2
+print(J)
+
+K= 3//5
+print(K)
+x= 3
+x *= 3
+print(x)
+x=3
+x **= 3
+print(x)
+
+J = 15
+M = 16
+print(M>J)
+print(J==M)
+print(J!=M)
+
+J=18
+M=20
+name="sam"
+print((M>=18) or(M>J) and (M!=21)and (name!="sam"))
+
+Message="nigga"
+#Check if "i" is seen in the Message
+print("i" in Message)
+
+
+
+
+
+
+
+
+
+
+
+
