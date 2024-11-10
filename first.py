119ea4cc190f223ff6dc62b85c9d33ba8c69cf5c
Kabeho_Samantha <samanthakabeho@gmail.com>
2024-11-10 20:42:32 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,34 @@
+## KABEHO SAMANTHA
+## M24B13/027  
+##B27433
+##0742739743
+##samanthakabeho@gmail.com
+
+
+
+
+
+## single line commenting
+
+
+## declaring variables
+name="Samantha"
+number_one =12
+number_two =3.14
+status = False
+
+print(type(number_one))
+print(type(number_two))
+print(number_one + number_two)
+
+def subtraction():
+    difference = number_one -number_two
+    return difference
+print(subtraction())
+
+## a void fuction
+def Addition():
+    sum = number_one + number_two
+    print(sum)
+    
+Addition()
